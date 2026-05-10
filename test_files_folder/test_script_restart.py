5b2301d22168301578a0ada881a26ecd09728ccb
sdryerbsu <sdryer@student.bridgew.edu>
2026-05-09 20:28:44 -0400

    added flake8 testing to pytest_script_restart.yml and fixed flake8 issues with files in test_files_folder

--- test_files_folder/test_script_restart.py
+++ test_files_folder/test_script_restart.py
@@ -2,14 +2,15 @@ import pytest
 from evennia.utils import create
 from evennia.scripts.models import ScriptDB
 
+
 @pytest.mark.django_db
 def test_script_restart_persists_active_state():
-    """
-    Test that calling start(force_restart=True) on a script
+    """Test that calling start(force_restart=True) on a script
     does not permanently set db_is_active to False.
     """
     # 1. Create a persistent TimeScript using Evennia's internal create wrapper
-    s = create.create_script('evennia.utils.gametime.TimeScript', interval=10, start_delay=False, repeats=-1)
+    s = create.create_script('evennia.utils.gametime.TimeScript',
+                             interval=10, start_delay=False, repeats=-1)
 
     # 2. Check the initial active state (Should be True)
     assert s.is_active is True
